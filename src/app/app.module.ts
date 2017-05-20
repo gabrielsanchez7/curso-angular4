950a1646351fd762058a5346bb4bc70a9eb3884f
Gabriel Sánchez <gslu2717@gmail.com>
2017-05-20 12:25:29 -0500

    Primer commit en home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
